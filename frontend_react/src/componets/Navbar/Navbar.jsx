920077237d1e15612173db1fd258394be1984313
Cmuhammad <originatorlabs@gmail.com>
2022-03-07 21:09:22 -0500

    images added will need to fix

--- frontend_react/src/componets/Navbar/Navbar.jsx
+++ frontend_react/src/componets/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './Navbar.scss';
+
 
 const Navbar = () => {
   return (
     <nav>
         <div>
-            <img src={images.logo} alt="logo"/>
+          <img src={images} alt="logo"/>
         </div>
     </nav>
   )
